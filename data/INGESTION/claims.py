485aeb61e618a49b1659536eefcb71f86a107a14
shaiksaidhul <157446745+shaiksaidhul@users.noreply.github.com>
2025-03-24 17:00:21 +0530

    updated

---
+++ data/INGESTION/claims.py
@@ -0,0 +1,22 @@
+# Databricks notebook source
+from pyspark.sql import SparkSession, functions as f
+
+BUCKET_NAME = "healthcare-bucket-22032025"
+BQ_TABLE = "avd-databricks-demo.bronze_dataset.claims"
+TEMP_GCS_BUCKET = "healthcare-bucket-22032025/temp/"
+
+claims_df=spark.read.csv(f"gs://{BUCKET_NAME}/landing/claims/*.csv",header=True)
+
+claims_df = claims_df.withColumn(
+    "datasource",
+    f.when(f.input_file_name().contains("hospital1"), "hosa").when(f.input_file_name().contains("hospital2"), "hosb")
+     .otherwise(None)
+)
+
+# Write DataFrame to BigQuery
+claims_df.write \
+    .format("bigquery") \
+    .option("table", BQ_TABLE) \
+    .option("temporaryGcsBucket", TEMP_GCS_BUCKET) \
+    .mode("overwrite") \
+    .save()
---
+++ data/INGESTION/cpt_codes.py
@@ -0,0 +1,22 @@
+# Databricks notebook source
+from pyspark.sql import SparkSession, functions as f
+
+BUCKET_NAME = "healthcare-bucket-22032025"
+BQ_TABLE = "avd-databricks-demo.bronze_dataset.cpt_codes"
+TEMP_GCS_BUCKET = "healthcare-bucket-22032025/temp/"
+
+# Read the CSV file
+cptcodes_df = spark.read.csv(f"gs://{BUCKET_NAME}/landing/cptcodes/*.csv", header=True)
+
+# Replace whitespaces in column names with underscores and convert to lowercase
+for col in cptcodes_df.columns:
+    new_col = col.replace(" ", "_").lower()
+    cptcodes_df = cptcodes_df.withColumnRenamed(col, new_col)
+
+# Write DataFrame to BigQuery
+cptcodes_df.write \
+    .format("bigquery") \
+    .option("table", BQ_TABLE) \
+    .option("temporaryGcsBucket", TEMP_GCS_BUCKET) \
+    .mode("overwrite") \
+    .save()
